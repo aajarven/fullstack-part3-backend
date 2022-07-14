b78af9e251685a44133f751102693549c1461a34
Anni Järvenpää <anni.jarvenpaa@gmail.com>
2022-07-14 07:53:22 +0000

    ex 3.11

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const app = express()
 
 app.use(cors())
+app.use(express.static('build'))
 app.use(express.json())
 
 morgan.token('data', function (req, res) { return req.method === "POST" ? JSON.stringify(req.body) : '' })
