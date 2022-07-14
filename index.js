361764ccaacee7cd7bea89bae3d9d924d90d1770
Anni Järvenpää <anni.jarvenpaa@gmail.com>
2022-07-14 06:26:02 +0000

    ex 3.8

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 
-app.use(morgan('tiny'))
 app.use(express.json())
+
+morgan.token('data', function (req, res) { return req.method === "POST" ? JSON.stringify(req.body) : '' })
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 
 let persons = [
   {
